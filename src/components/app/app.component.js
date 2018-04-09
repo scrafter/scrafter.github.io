24e7136a5a5b5a122460993009df693cbbae128a
tomaszmanka <tomasz.manka@thesoftwarehouse.pl>
2018-04-09 21:07:44 +0200

    components skeleton added

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styles from './app.scss';
+import Landing from '../landing/landing.component';
 
 export default class App extends Component {
   render() {
     return (
       <div className={styles.appContainer}>
+        <Landing/>
       </div>
     );
   }
